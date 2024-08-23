7051e888f80d213809ce3aebc496b35e16d534b5
Thor ✨ <hello@thor.computer>
2024-08-23 20:30:39 +0200

    Added share-intent

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,51 +1,33 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import { Slot, useRouter } from "expo-router";
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-import { useShareIntent, ShareIntentFile } from "expo-share-intent";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { ShareIntentProvider } from "expo-share-intent";
 
 export default function RootLayout() {
+  const router = useRouter();
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  const { hasShareIntent, shareIntent, resetShareIntent, error } =
-  useShareIntent({
-    debug: true,
-    resetOnBackground: true,
-  });
-
-  useEffect(() => {
-    if (hasShareIntent && shareIntent) {
-      console.log('Received shared content:', shareIntent);
-    }
-  }, [hasShareIntent, shareIntent]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <ShareIntentProvider
+      options={{
+        debug: true,
+        resetOnBackground: true,
+        onResetShareIntent: () =>
+          // used when app going in background and when the reset button is pressed
+          router.replace({
+            pathname: "/",
+          }),
+      }}
+    >
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Slot />
+      </ThemeProvider>
+    </ShareIntentProvider>
   );
 }
